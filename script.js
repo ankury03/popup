8974a4b5214f627cc27d48366df4381d6c6bda2b
ankury03 <120250151+ankury03@users.noreply.github.com>
2024-09-22 11:50:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const Popup = document.querySelector(".popup");
+
+function openPopup(){
+    Popup.classList.add("openPopup");
+}
+
+function closePopup(){
+    Popup.classList.remove("openPopup");
+}
